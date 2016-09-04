0d8c7befb34b7e2dcadd7ec4fdfca02ab57a6557
Christian Würthner <christian@savedroid.de>
2016-09-04 11:22:14 +0200

    Added getSuffix()

--- app/src/main/java/de/crysxd/mobilefitness/data/MfUnit.java
+++ app/src/main/java/de/crysxd/mobilefitness/data/MfUnit.java
@@ -4,5 +4,23 @@ package de.crysxd.mobilefitness.data;
  * A enum for common units
  */
 public enum MfUnit {
-    KG, SECONDS, METER, UNDEFINED
+    KG, SECONDS, METER, UNDEFINED;
+
+    /**
+     * Returns the suffix for this unit
+     *
+     * @return the suffix
+     */
+    public String getSuffix() {
+        switch (this) {
+            case KG:
+                return "kg";
+            case SECONDS:
+                return "s";
+            case METER:
+                return "m";
+            default:
+                return "";
+        }
+    }
 }
